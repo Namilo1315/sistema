fbf6fa8600dc95ae123f8f57adb8bd2e3a5174e8
nalopez <natalia89751315@gmail.com>
2025-07-27 12:18:57 -0300

    mas

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'juzgado-vial-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/juzgado.html',
+  '/notificaciones.html',
+  '/style.css',
+  
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
